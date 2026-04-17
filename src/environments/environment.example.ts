ee46484d6f04ebbe9c863a5f76b11f6e5b683964
Yehor Petrov <yehorpetrov5@gmail.com>
2026-04-17 10:26:25 +0100

    feat: interceptor added

---
+++ src/environments/environment.example.ts
@@ -0,0 +1,7 @@
+// create two files environment.development.ts & environment.ts and put this config here:
+export const environment = {
+  production: true,
+  apiUrl: 'https://api.thecatapi.com',
+  API_KEY: 'PUT_YOUR_API_KEY_HERE', // ‼️REQUIRED in BOTH FILES
+};
+// TO GET YOUR API KEY, GO TO: https://thecatapi.com/
